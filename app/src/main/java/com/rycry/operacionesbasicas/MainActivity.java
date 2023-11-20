674e4a168ed2b2d1782abaf546f663d7164bfbbe
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-11-19 23:28:07 -0500

    creacion layout RESTA

--- app/src/main/java/com/rycry/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/rycry/operacionesbasicas/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_suma);
 
         edNum1 = findViewById(R.id.edNum1);
         edNum2 = findViewById(R.id.edNum2);
