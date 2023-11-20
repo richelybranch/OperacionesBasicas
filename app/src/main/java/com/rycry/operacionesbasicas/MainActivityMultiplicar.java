1b75d05f41fe6e428176cbcfe93551a69bd603ec
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-11-20 00:32:36 -0500

    creacion layout MULTIPLICACION

---
+++ app/src/main/java/com/rycry/operacionesbasicas/MainActivityMultiplicar.java
@@ -0,0 +1,21 @@
+package com.rycry.operacionesbasicas;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityMultiplicar extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resta);
+
+
+    }
+}
