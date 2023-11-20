543d6c632a910c7eba4810db420db91cfdae89ee
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-11-20 00:29:46 -0500

    creacion activity RESTA

--- app/src/main/java/com/rycry/operacionesbasicas/MainActivityRestar.java
+++ app/src/main/java/com/rycry/operacionesbasicas/MainActivityRestar.java
@@ -1,13 +1,14 @@
 package com.rycry.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityRestar extends AppCompatActivity {
 
     private EditText edNum1, edNum2;
     private TextView tvResultado;
@@ -15,32 +16,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_suma);
+        setContentView(R.layout.activity_resta);
 
         edNum1 = findViewById(R.id.edNum1);
         edNum2 = findViewById(R.id.edNum2);
         tvResultado = findViewById(R.id.tvResultado);
-        Button btnSumar = findViewById(R.id.btnSumar);
+        Button btnRestar = findViewById(R.id.btnRestar);
 
-        btnSumar.setOnClickListener(new View.OnClickListener() {
+        btnRestar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sumarNumeros();
+                restarNumeros();
             }
         });
     }
 
-    private void sumarNumeros() {
+    private void restarNumeros() {
         try {
             // Obtener los valores ingresados
             double num1 = Double.parseDouble(edNum1.getText().toString());
-            int num2 = Integer.parseInt(edNum2.getText().toString());
-            double resultado=num1+num2;
-            tvResultado.setText(resultado+"");
+            double num2 = Double.parseDouble(edNum2.getText().toString());
 
+            // Realizar la resta
+            double resultado = num1 - num2;
+
+            // Mostrar el resultado en el TextView
+            tvResultado.setText("Resultado: " + resultado);
         } catch (NumberFormatException e) {
             // Manejar el caso en que la entrada no sea un número válido
             tvResultado.setText(":(");
         }
     }
-}
+    }
---
+++ app/src/main/java/com/rycry/operacionesbasicas/MainActivitySumar.java
@@ -0,0 +1,46 @@
+package com.rycry.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivitySumar extends AppCompatActivity {
+
+    private EditText edNum1, edNum2;
+    private TextView tvResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+
+        edNum1 = findViewById(R.id.edNum1);
+        edNum2 = findViewById(R.id.edNum2);
+        tvResultado = findViewById(R.id.tvResultado);
+        Button btnSumar = findViewById(R.id.btnRestar);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sumarNumeros();
+            }
+        });
+    }
+
+    private void sumarNumeros() {
+        try {
+            // Obtener los valores ingresados
+            double num1 = Double.parseDouble(edNum1.getText().toString());
+            int num2 = Integer.parseInt(edNum2.getText().toString());
+            double resultado=num1+num2;
+            tvResultado.setText(resultado+"");
+
+        } catch (NumberFormatException e) {
+            // Manejar el caso en que la entrada no sea un número válido
+            tvResultado.setText(":(");
+        }
+    }
+}
